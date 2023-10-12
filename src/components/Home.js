e02d4c0f0cb4065bdccf986fa341f1c25597dc3b
Lana W <lanawehbeh12@gmail.com>
2023-10-11 22:46:37 -0400

    Updated ESLint Error Configuration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import Container from "react-bootstrap/Container";
-import Button  from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 
 const Home = ({information}) => {
-
     const returnInfo = async () => {
         let personChosen = document.getElementById("personChosen").value;
         const url = "https://pdflex-backend.duckdns.org/information/" + personChosen;
         try {
             const res = await fetch(url, {method: "GET"});
-            const data = await res.json();gi
+            const data = await res.json();
             console.log(data);
-            headingUpdated.innerHTML = "Here ya go! ";
+            /*eslint no-undef: "off"*/
             toBeUpdated.innerHTML = JSON.stringify(data);
         } catch (err) {
             console.log(err);
@@ -20,8 +19,7 @@ const Home = ({information}) => {
     return (
         <div>
             <Container>
-                <p id="headingUpdated"></p>
-                <p id="toBeUpdated"></p>
+                <p id="toBeUpdated"> </p>
                 <input id="personChosen" type="text" placeholder='Insert Name Here:'></input>
                 <Button onClick={returnInfo}> Learn More About Me </Button>
                 <br></br>
