6b628c5e16af6a395463f056e46adf8b87565cef
Lana W <lanawehbeh12@gmail.com>
2023-10-11 22:22:32 -0400

    Added Additional Files

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+import Button  from "react-bootstrap/Button";
+
+const Home = ({information}) => {
+
+    const returnInfo = async () => {
+        let personChosen = document.getElementById("personChosen").value;
+        const url = "https://pdflex-backend.duckdns.org/information/" + personChosen;
+        try {
+            const res = await fetch(url, {method: "GET"});
+            const data = await res.json();gi
+            console.log(data);
+            headingUpdated.innerHTML = "Here ya go! ";
+            toBeUpdated.innerHTML = JSON.stringify(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    return (
+        <div>
+            <Container>
+                <p id="headingUpdated"></p>
+                <p id="toBeUpdated"></p>
+                <input id="personChosen" type="text" placeholder='Insert Name Here:'></input>
+                <Button onClick={returnInfo}> Learn More About Me </Button>
+                <br></br>
+            </Container>
+        </div>
+    )
+
+}
+export default Home
