8500ffa1bce94be846080ee6af866b3f3e4a61c1
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-23 10:50:13 -0500

    Organized files and routing

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
 import './App.css';
-import api from './api/axiosConfig';
-import {useState, useEffect} from 'react';
-import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
-// import Header from './components/Header';
-import Form from './components/Form';
 import Login from './components/Login';
 
 function App() {
 
-    const [information, setInformation] = useState();
-    // const [info, setInfo] = useState();
-    const getInformation = async () => {
-
-        try
-        {
-            const response = await api.get("/information");
-
-            // console.log(response.data);
-
-            setInformation(response.data);
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-
-    // const getInformationData = async (firstName) => {
-    //     try
-    //     {
-    //         const response = await api.get("/information/${firstName}");
-    //
-    //         const singleInfo = response.data;
-    //
-    //         setInfo(singleInfo);
-    //     }
-    //     catch (error) {
-    //
-    //     }
-    // }
-
-    useEffect(() => {
-        getInformation().then(r => {});
-    }, [])
-
     return (
         <div className="App">
-            {/* <Header/> */}
             <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route path="/" element={<Home information = {information}/>} ></Route>;
-                    <Route path="/Information/:firstName" element={<Home GetInformationData={information}/>}></Route>;
-                </Route>
-                <Route path="/form" element={<Form/>}></Route>;
-                <Route path="/login" element={<Login/>}></Route>;
-
+                <Route path="/" element={<Login/>}></Route>;
             </Routes>
         </div>
     );
